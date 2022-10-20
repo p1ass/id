77d8e20b5a44e5893f5cc6f5dda61e88054e762a
p1ass <naoki.kishi@dena.com>
2022-10-20 23:05:19 +0900

    [backend] add first step oidc server implements

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/p1ass/id/backend/gen/oidc/v1/oidcv1connect"
 
-	"golang.org/x/oauth2"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	"github.com/p1ass/id/backend/oidc"
 )
 
-type foo struct {
-}
-
-var _ oidcv1connect.AuthenticationServiceHandler = &foo{}
-
 func main() {
-	fmt.Println(oauth2.Config{})
+	server := oidc.NewOIDCServer()
 
+	mux := http.NewServeMux()
+	path, handler := oidcv1connect.NewOIDCPrivateServiceHandler(server)
+	mux.Handle(path, handler)
+
+	err := http.ListenAndServe(
+		"localhost:8080",
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+	if err != nil {
+		log.Println(err)
+	}
 }
