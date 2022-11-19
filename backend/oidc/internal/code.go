8e661fd73cfd0fda76e63af9f7ce5c8b4295b6e9
p1ass <naoki.kishi@dena.com>
2022-11-19 23:34:16 +0900

    [backend] add authorization code generate logic

---
+++ backend/oidc/internal/code.go
@@ -0,0 +1,77 @@
+package internal
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+	"net/url"
+	"time"
+
+	"github.com/Songmu/flextime"
+)
+
+// AuthorizationCode is a authorization code defined by [RFC 6749 Section 4.1.2].
+// For security consideration in [RFC 6819 Section 5.2.4], authorization code binds to clientID and redirectURI.
+//
+// [RFC 6749 Section 4.1.2]: https://www.rfc-editor.org/rfc/rfc6749#section-4.1.2
+// [RFC 6819 Section 5.2.4]: https://www.rfc-editor.org/rfc/rfc6819#section-5.2.4
+type AuthorizationCode struct {
+	Code        string
+	clientID    string
+	redirectURI url.URL
+	issued      time.Time
+	expired     time.Time
+}
+
+type UsedAuthorizationCode struct {
+}
+
+const (
+	authorizationCodeByteLength = 32
+	// The authorization Code MUST expire shortly after it is issued to mitigate the risk of leaks.
+	// A maximum authorization code lifetime of 10 minutes is RECOMMENDED.
+	// In this system, lifetime sets 30 seconds.
+	authorizationCodeExpiration = 30 * time.Second
+)
+
+var (
+	ErrCodeIsExpired = errors.New("code is expired")
+)
+
+func NewAuthorizationCode(client *Client, redirectURI url.URL) *AuthorizationCode {
+	authorizationCode := make([]byte, authorizationCodeByteLength)
+	_, err := io.ReadFull(rand.Reader, authorizationCode)
+	if err != nil {
+		panic(err)
+	}
+
+	now := flextime.Now().UTC()
+	return &AuthorizationCode{
+		Code:        base64.RawURLEncoding.WithPadding(base64.NoPadding).EncodeToString(authorizationCode),
+		clientID:    client.ID,
+		redirectURI: redirectURI,
+		issued:      now,
+		expired:     now.Add(authorizationCodeExpiration),
+	}
+}
+
+func (c *AuthorizationCode) Expired() bool {
+	now := flextime.Now().UTC()
+	return now.After(c.expired)
+}
+
+// TODO: implement
+func (c *AuthorizationCode) Use() (*UsedAuthorizationCode, error) {
+	if c.Expired() {
+		return nil, ErrCodeIsExpired
+	}
+
+	// TODO: If an authorization code is used more than
+	//         once, the authorization server MUST deny the request and SHOULD
+	//         revoke (when possible) all tokens previously issued based on
+	//         that authorization code.
+	// https://www.rfc-editor.org/rfc/rfc6749#section-4.1.2
+
+	return &UsedAuthorizationCode{}, nil
+}
---
+++ backend/oidc/internal/code_datestore.go
@@ -0,0 +1,50 @@
+package internal
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"sync"
+)
+
+var ErrCodeNotFound = errors.New("code not found")
+
+type CodeDatastore interface {
+	// Fetch fetches AuthorizationCode witch is not expired.
+	// When code is expired, returns ErrCodeIsExpired error.
+	Fetch(code, clientID string, redirectURI url.URL) (*AuthorizationCode, error)
+	Save(code *AuthorizationCode) error
+}
+
+type inMemoryCodeDatastore struct {
+	datastore sync.Map
+}
+
+func NewInMemoryCodeDatastore() CodeDatastore {
+	return &inMemoryCodeDatastore{
+		datastore: sync.Map{},
+	}
+}
+
+func (d *inMemoryCodeDatastore) Fetch(code, clientID string, redirectURI url.URL) (*AuthorizationCode, error) {
+	value, ok := d.datastore.Load(d.key(code, clientID, redirectURI))
+	if !ok {
+		return nil, ErrCodeNotFound
+	}
+	gotCode := value.(*AuthorizationCode)
+	if gotCode.Expired() {
+		return nil, ErrCodeIsExpired
+	}
+	return gotCode, nil
+
+}
+
+func (d *inMemoryCodeDatastore) Save(code *AuthorizationCode) error {
+	d.datastore.Store(d.key(code.Code, code.clientID, code.redirectURI), code)
+	return nil
+}
+
+// key for only in-memory datastore
+func (d *inMemoryCodeDatastore) key(code, clientID string, redirectURI url.URL) string {
+	return fmt.Sprintf("%s-%s-%s", clientID, code, redirectURI.String())
+}
