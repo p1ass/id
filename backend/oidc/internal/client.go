fa0ae4d3b59e8c0ebb06f4851c68b410c132440d
p1ass <naoki.kishi@dena.com>
2022-11-19 16:10:23 +0900

    [backend] add redirect uri validation logic

--- backend/oidc/internal/client.go
+++ backend/oidc/internal/client.go
@@ -1,5 +1,12 @@
 package internal
 
+import (
+	"errors"
+	"net/url"
+)
+
+var ErrNotIdenticalRedirectURI = errors.New("not identical redirect uri")
+
 // Client represents OAuth 2.0 client.
 type Client struct {
 	// ID is a unique string  and is exposed to public.
@@ -7,4 +14,17 @@ type Client struct {
 	// hashedPassword is used for HTTP Basic Authentication Scheme [RFC2617].
 	// [RFC2617]: https://www.rfc-editor.org/rfc/rfc2617.html
 	hashedPassword HashedPassword
+
+	// redirectURIs are absolute URIs.
+	// [RFC6749 Section3.1.2]: https://www.rfc-editor.org/rfc/rfc6749#section-3.1.2
+	redirectURIs []url.URL
+}
+
+func (c *Client) IdenticalRedirectURI(redirectURI url.URL) error {
+	for _, uri := range c.redirectURIs {
+		if uri == redirectURI {
+			return nil
+		}
+	}
+	return ErrNotIdenticalRedirectURI
 }
---
+++ backend/oidc/internal/client_test.go
@@ -0,0 +1,84 @@
+package internal
+
+import (
+	"net/url"
+	"testing"
+)
+
+func TestClient_IdenticalRedirectURI(t *testing.T) {
+	t.Parallel()
+
+	type fields struct {
+		redirectURIs []url.URL
+	}
+	type args struct {
+		redirectURI url.URL
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "when redirectURI is registered, return nil",
+			fields: fields{
+				redirectURIs: []url.URL{
+					mustURLParse("https://web.test/callback"),
+					mustURLParse("https://web.test/callback2"),
+				},
+			},
+			args: args{
+				redirectURI: mustURLParse("https://web.test/callback"),
+			},
+			wantErr: false,
+		},
+		{
+			name: "when redirectURI is not registered, return err",
+			fields: fields{
+				redirectURIs: []url.URL{
+					mustURLParse("https://web.test/callback"),
+					mustURLParse("https://web.test/callback2"),
+				},
+			},
+			args: args{
+				redirectURI: mustURLParse("https://web.test/not-registered"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "when redirectURI matches partial, return err",
+			fields: fields{
+				redirectURIs: []url.URL{
+					mustURLParse("https://web.test"),
+					mustURLParse("https://web.test"),
+				},
+			},
+			args: args{
+				redirectURI: mustURLParse("https://web.test/partial-match"),
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			c := &Client{
+				redirectURIs: tt.fields.redirectURIs,
+			}
+			if err := c.IdenticalRedirectURI(tt.args.redirectURI); (err != nil) != tt.wantErr {
+				t.Errorf("IdenticalRedirectURI() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func mustURLParse(rawURL string) url.URL {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		panic(err)
+	}
+	return *u
+}
