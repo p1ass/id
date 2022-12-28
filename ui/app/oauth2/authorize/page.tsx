cc4b443ca053166086374f74d4f1425a6587b7a3
p1ass <naoki.kishi@dena.com>
2022-12-28 23:55:10 +0900

    call connect-web api temporary

--- ui/app/oauth2/authorize/page.tsx
+++ ui/app/oauth2/authorize/page.tsx
@@ -1,4 +1,28 @@
-const AuthorizePage = () => {
+'use client'
+import {
+  createConnectTransport,
+  createPromiseClient,
+} from "@bufbuild/connect-web";
+import {OIDCPrivateService} from "../../../gen/oidc/v1/oidc_connectweb";
+import {useEffect} from "react";
+
+const transport = createConnectTransport({
+  baseUrl: "http://localhost:8080",
+});
+
+// Here we make the client itself, combining the service
+// definition with the transport.
+const client = createPromiseClient(OIDCPrivateService, transport);
+
+const AuthorizePage =  () => {
+  useEffect(()=>{
+    const authenticateAsync = async ()=>{
+      const res = await client.authenticate({
+      })
+      console.log(res)
+    }
+    authenticateAsync()
+  },[])
   return <div>
     <h1>Authorize</h1>
   </div>
