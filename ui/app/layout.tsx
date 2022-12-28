c80ee3036379fa625c69b183964732e8b94ef379
p1ass <naoki.kishi@dena.com>
2022-12-28 23:07:46 +0900

    add /oauth2/authorize page

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -11,7 +11,9 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head >
+        <title>p1ass ID</title>
+      </head>
       <body>{children}</body>
     </html>
   )
