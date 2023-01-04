d1d838b808ebc095b87efb8ea576a64e3d4324c5
p1ass <naoki.kishi@dena.com>
2023-01-04 18:06:39 +0900

    user tailwind css

--- ui/app/oauth2/authorize/consent/page.tsx
+++ ui/app/oauth2/authorize/consent/page.tsx
@@ -8,6 +8,10 @@ type PageProps = {
   searchParams?: { [key: string]: string | string[] | undefined }
 }
 
+const labelClassName = 'mb-1 block pr-4 font-bold text-gray-500 md:mb-0'
+const inputClassName =
+  'w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none'
+
 const AuthorizePage = async ({ searchParams }: PageProps) => {
   if (!searchParams) {
     return notFound()
@@ -22,35 +26,51 @@ const AuthorizePage = async ({ searchParams }: PageProps) => {
   const parameter = parsed.data
 
   return (
-    <div>
-      <h2>Consent Page</h2>
-      <form action={buildAuthorizePath()} method="post">
-        <div>
-          <label htmlFor="client_id">client_id: </label>
-          <input name="client_id" value={parameter.client_id} />
+    <div className="m-8">
+      <h2 className="mb-4 text-xl">Consent Page</h2>
+      <form action={buildAuthorizePath()} method="post" className="w-full max-w-sm">
+        <div className="mb-4 md:flex md:items-center">
+          <label className={labelClassName} htmlFor="client_id">
+            client_id:{' '}
+          </label>
+          <input name="client_id" value={parameter.client_id} className={inputClassName} />
         </div>
-        <div>
-          <label htmlFor="redirect_uri">redirect_uri: </label>
-          <input name="redirect_uri" value={parameter.redirect_uri} />
+        <div className="mb-4 md:flex md:items-center">
+          <label className={labelClassName} htmlFor="redirect_uri">
+            redirect_uri:{' '}
+          </label>
+          <input name="redirect_uri" value={parameter.redirect_uri} className={inputClassName} />
         </div>
-        <div>
-          <label htmlFor="scope">scope: </label>
-          <input name="scope" value={parameter.scope} />
+        <div className="mb-4 md:flex md:items-center">
+          <label htmlFor="scope" className={labelClassName}>
+            scope:{' '}
+          </label>
+          <input name="scope" value={parameter.scope} className={inputClassName} />
         </div>
-        <div>
-          <label htmlFor="state">state: </label>
-          <input name="state" value={parameter.state} />
+        <div className="mb-4 md:flex md:items-center">
+          <label htmlFor="state" className={labelClassName}>
+            state:{' '}
+          </label>
+          <input name="state" value={parameter.state} className={inputClassName} />
         </div>
-        <div>
-          <label htmlFor="response_type">response_type: </label>
-          <input name="response_type" value={parameter.response_type} />
+        <div className="mb-4 md:flex md:items-center">
+          <label htmlFor="response_type" className={labelClassName}>
+            response_type:{' '}
+          </label>
+          <input name="response_type" value={parameter.response_type} className={inputClassName} />
         </div>
-        <div>
-          <label htmlFor="consent">consent: </label>
-          <input name="consent" value="true" />
+        <div className="mb-4 md:flex md:items-center">
+          <label htmlFor="consent" className={labelClassName}>
+            consent:{' '}
+          </label>
+          <input name="consent" value="true" className={inputClassName} />
         </div>
-        <div>
-          <input type="submit" value="Consent" />
+        <div className="mb-4 md:flex md:items-center">
+          <input
+            type="submit"
+            className="rounded bg-purple-500 py-2 px-4 font-bold text-white shadow hover:bg-purple-400 focus:outline-none"
+            value="Consent"
+          />
         </div>
       </form>
     </div>
--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -5,8 +5,9 @@ import { buildAuthorizeUri } from '../pages/api/oauth2/route'
 export default function Home() {
   return (
     <main>
-      <h2>Endpoint Links for Debug</h2>
+      <h2 className="text-3xl">Endpoint Links for Debug</h2>
       <Link
+        className="text-blue-900 underline"
         href={buildAuthorizeUri({
           client_id: 'dummy_client_id',
           redirect_uri: 'https://localhost:8443/test/a/local/callback',
