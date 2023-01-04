eef86c710c38857d2419797ab3c86203d9def1a1
p1ass <naoki.kishi@dena.com>
2023-01-04 17:50:11 +0900

    add theme setting

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true
+    },
+    fontFamily: {
+      sans: [
+        'Hiragino Kaku Gothic ProN',
+        'Hiragino Sans',
+        'Segoe UI',
+        'Roboto',
+        'Noto Sans CJK JP',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Sans Emoji'
+      ]
+    },
     extend: {}
   },
   plugins: []
